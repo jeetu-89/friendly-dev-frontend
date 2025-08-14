1bf0a4374aa614f84819834ca4ba43e200305192
jeetu <jitendradew2003@gmail.com>
2025-08-14 11:53:06 +0530

    Fetching data for project using loaders

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,17 @@
+import type { Route } from "./+types/index";
+import type { Project } from "~/types";
 
-const ProjectsPage = () => {
+export async function loader({request}:Route.LoaderArgs): Promise<{projects: Project[]}> {
+    const res = await fetch("http://localhost:8000/projects");
+    const data = await res.json();
+
+    return {projects: data}
+}
+
+const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
+
+    const {projects} = loaderData as {projects: Project[]};
+    console.log(projects);
     return ( 
         <>
             <h2 className="mb-8 text-3xl font-bold text-center">
@@ -8,5 +20,5 @@ const ProjectsPage = () => {
         </>
      );
 }
- 
+  
 export default ProjectsPage;
