e4f2e28d0471e520ff49d5117fbb17938b1fbcf8
jeetu <jitendradew2003@gmail.com>
2025-08-16 18:27:53 +0530

    Add action -> on clicking submit displaying a green box at top

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,9 +1,36 @@
 import { Form } from "react-router";
+import type { Route } from "./+types";
+import { div } from "framer-motion/client";
 
-const ContactPage = () => {
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const subject = formData.get("subject");
+  const message = formData.get("message");
+
+  const data = {
+    name,
+    email,
+    subject,
+    message,
+  };
+
+  return { message: "Form Successfully Submitted", data };
+}
+
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
   return (
     <div className="max-w-3xl bg-gray-900 py-10 px-6 rounded">
       <h2 className="text-3xl font-bold text-center mb-8">Contact me</h2>
+      
+      {actionData?.message ? (
+        <div className="mb-6 bg-green-600 text-center py-2 rounded-md font-bold">
+          {actionData.message}
+        </div>
+      ) : null}
+
       <Form method="post" className="space-y-6">
         {/* Name */}
         <div>
@@ -67,7 +94,9 @@ const ContactPage = () => {
             className="bg-gray-800 w-full px-6 py-2 rounded-lg focus:outline-0 border border-gray-700"
           />
         </div>
-        <button className="bg-blue-600 py-2 w-full rounded-md hover:bg-blue-700 cursor-pointer transition">Send message</button>
+        <button className="bg-blue-600 py-2 w-full rounded-md hover:bg-blue-700 cursor-pointer transition">
+          Send message
+        </button>
       </Form>
     </div>
   );
